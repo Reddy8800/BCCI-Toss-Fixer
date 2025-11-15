4055f0068ba57b92f247100bcbc42e617657657b
ankit nishad <ankitnishad3333@gmail.com>
2025-11-15 11:50:27 +0530

    final code with bg

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-// Countdown Timer (45 seconds loop)
-let time = 45;
+// Countdown Timer (60 seconds loop)
+let time = 60;
 
 function updateTimer() {
   document.getElementById("timer").textContent = time + "s";
 
   time--;
   if (time < 0) {
-    time = 45;
+    time = 60;
   }
 }
 
